0f9a0674b2c4975db05ddab25c92bd4db736f47c
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-26 15:48:25 -0700

    WIP: testing function that deals with words that start with 'qu'
    Co-authored-by: Luke Callahan  <lukemcal@gmail.com>
    Co-authored-by: Ruben Giosa <rubenalexgiosa@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,27 @@ function convertToArrayAndCheck(text) {
   for (let i = 0; i < textArray.length; i += 1) {
     firstLetter = textArray[i].charAt(0);
     if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
-      
-        vowelWord = textArray[i] + "way";
-        console.log(vowelWord);
-      };
+      vowelWord = textArray[i] + "way";
+      textArray[i] = vowelWord;
+      console.log(vowelWord);
+    } else {
+      consonantWord = textArray[i];
+      textArray[i] = consonantChange(consonantWord)
+    }
   }
 }
 
+function consonantChange(consonantWord) {
+  // let letterArray = consonantWord.split("");
+  if (consonantWord.charAt(0) === "q" && consonantWord.charAt(1) === "u") {
+    let preFix =  consonantWord.slice(0, 2);
+    console.log(preFix);
+  } else if (consonantWord.charAt(1) === "q" && consonantWord.charAt(2) === "u") {
+    let preFix =  consonantWord.slice(0, 2);
+    pigLatinWord = consonantWord + preFix + "ay";
+  } 
+    textArray[i]= consonantWord + preFix + "ay";
+    console.log(pigLatinWord);
+}
 
 
