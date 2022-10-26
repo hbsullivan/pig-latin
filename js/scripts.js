7706025f67746a0a136238f72a56f14435f4d378
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-26 15:23:01 -0700

    pass test that adds 'way' to end of words that start with a vowel
    Co-authored-by: Ruben Giosa <rubenalexgiosa@gmail.com>
    Co-authored-by: Luke Callahan  <lukemcal@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
+const vowelArray = ["a","e","i","o","u"];
+
 function convertToArrayAndCheck(text) {
-  textArray = text.split(" ");
+  let textArray = text.split(" ");
   console.log(textArray);
+  for (let i = 0; i < textArray.length; i += 1) {
+    firstLetter = textArray[i].charAt(0);
+    if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
+      
+        vowelWord = textArray[i] + "way";
+        console.log(vowelWord);
+      };
+  }
 }
+
+
+
